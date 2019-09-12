a1ad383c74d79e6e62b0844388914d40fe1e975c
Max Kasprzik <m.kasprzik04@gmail.com>
2019-09-12 08:49:24 -0700

    fixed formatting; expandable background

---
+++ dndapp/static/script.js
@@ -0,0 +1,15 @@
+// $( document ).ready(function() {
+//     function myFunction() {
+//         alert("hi");
+//         var x = document.getElementById('upload-charpic-btn');
+//         if (x.style.display === 'none') {
+//             x.style.visibility = 'block';
+//         } else {
+//             x.style.visibility = 'none';
+//         }
+//     }
+//
+//     $( "#edit-pic-btn" ).click(function(){
+//         myFunction();
+//     });
+// });
